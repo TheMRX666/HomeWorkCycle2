3485c682e316b7b3f6dee5501c1d6b92760ad084
TheMRX666 <110379933+TheMRX666@users.noreply.github.com>
2022-11-11 00:21:52 +0200

    Update Main.java

--- a3c3/src/a3c3/Main.java
+++ a3c3/src/a3c3/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 			int count = 0;
-			for(int i = 0; i<=5000; i++) {
+			for(int i = 0; i<=50000; i++) {
 			String num = Integer.toString(i);		
 		    String strArr[] = num.split("");
 		     int numArr[] = new int[strArr.length];
